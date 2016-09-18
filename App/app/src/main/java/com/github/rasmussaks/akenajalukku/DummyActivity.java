b4e45412179525bf444931ee826707346db34582
Rasmus Saks <bogeyman.est@gmail.com>
2016-09-18 16:34:37 +0300

    Set up initial Android app code. Fixes #3

---
+++ App/app/src/main/java/com/github/rasmussaks/akenajalukku/DummyActivity.java
@@ -0,0 +1,13 @@
+package com.github.rasmussaks.akenajalukku;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DummyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy);
+    }
+}
