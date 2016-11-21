8488fa23037edc131d2077f644452024ed3bf26b
Rezonation <TheAoihoshi@gmail.com>
2016-11-21 21:02:01 +0200

    Add poi and journey creation routes

--- CMS/routes/index.js
+++ CMS/routes/index.js
@@ -5,8 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/poi/new', function(req, res, next) {
+  res.render('poi', { id: -1});
+});
 router.get('/poi/:id', function(req, res, next) {
   res.render('poi', { id: req.params.id});
+});
+router.get('/journey/new', function(req, res, next) {
+  res.render('journey', { id: -1 });
 });
 router.get('/journey/:id', function(req, res, next) {
   res.render('journey', { id: req.params.id });
