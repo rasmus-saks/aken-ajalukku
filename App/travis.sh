43d22be4105e6b5526fb55b7a7bd3564f17ecec7
Rasmus Saks <bogeyman.est@gmail.com>
2016-11-21 17:40:56 +0200

    Try to fix Android build on Travis

--- App/travis.sh
+++ App/travis.sh
@@ -1,8 +1,8 @@
 set -e
-.gradlew build
+chmod 777 ./gradlew
+./gradlew build
 echo no | android create avd --force -n test -t android-$ANDROID_EMULATOR_API_LEVEL --abi google_apis/armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
-chmod 777 ./gradlew
 ./gradlew connectedCheck
