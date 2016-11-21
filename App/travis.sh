45b365949024aab684bf3db144d5f1c07076487e
Rasmus Saks <bogeyman.est@gmail.com>
2016-11-21 16:18:45 +0200

    Try to fix Android build on Travis

--- App/travis.sh
+++ App/travis.sh
@@ -1,3 +1,4 @@
+android list targets
 echo no | android create avd --force -n test -t android-$ANDROID_API_LEVEL --abi $ANDROID_ABI
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
