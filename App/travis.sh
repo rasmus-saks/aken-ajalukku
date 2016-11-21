6f5ea7dd49172555d1a8edf7f03641317e453b61
Rasmus Saks <bogeyman.est@gmail.com>
2016-11-21 16:33:34 +0200

    Try to fix Android build on Travis

--- App/travis.sh
+++ App/travis.sh
@@ -1,6 +1,6 @@
 set -e
 android list targets
-echo no | android create avd --force -n test -t android-$ANDROID_EMULATOR_API_LEVEL --abi default/armeabi-v7a
+echo no | android create avd --force -n test -t android-$ANDROID_EMULATOR_API_LEVEL --abi google_apis/armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
