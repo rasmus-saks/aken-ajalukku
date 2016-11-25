3ddb23d87651deebd7e8f624ecf89cf0a1f15a59
Rasmus Saks <bogeyman.est@gmail.com>
2016-11-26 00:18:34 +0200

    Add several checks to API and corresponding test cases Disallow PoI DELETE if the PoI is part of an existing journey. Validate PoI lists given to PUT and POST /api/journey

--- CMS/test/api.js
+++ CMS/test/api.js
@@ -3,14 +3,14 @@ var express = require('express');
 var app = require('../main');
 var should = require("should");
 var auth = "Basic dXNlcm5hbWU6cGFzc3dvcmQ=";
- 
+
 function apiRequest(method, path) {
   let r = request(app);
   return r[method]
     .call(r, path)
     .set("Authorization", auth)
     .expect("Content-Type", /json/);
- 
+
 }
 describe("PoI API", function () {
   describe("DELETE /api/data", function () {
@@ -30,7 +30,7 @@ describe("PoI API", function () {
         });
     });
   });
- 
+
   describe("POST /api/poi", function () {
     it("should require authorization", function (done) {
       request(app)
@@ -76,7 +76,7 @@ describe("PoI API", function () {
         });
     });
   });
- 
+
   describe("GET /api/poi", function () {
     it("should require id", function (done) {
       apiRequest("get", "/api/poi")
@@ -113,19 +113,31 @@ describe("Journey API", function () {
         .send({title: {EN: "hi", ET: "hei"}, description: {EN: "bye", ET: "nägemist"}})
         .expect(400, done);
     });
-    it("should add a new journey", function (done) {
+    it("should require valid PoI list", function (done) {
       apiRequest("post", "/api/journey")
-        .expect(200)
         .send({title: {EN: "hi", ET: "hei"}, description: {EN: "bye", ET: "nägemist"}, pois: [0, 1]})
+        .expect(400, done);
+    });
+    it("should add a new journey", function (done) {
+      apiRequest("post", "/api/poi")
+        .expect(200)
+        .send({title: {EN: "hi2", ET: "hei2"}, description: {EN: "bye2", ET: "nägemist2"}})
         .end(function (err, res) {
-          res.body.should.be.eql({
-            title: {EN: "hi", ET: "hei"},
-            description: {EN: "bye", ET: "nägemist"},
-            pois: [0, 1],
-            id: 0
-          });
-          done(err);
+          if (err) done(err);
+          apiRequest("post", "/api/journey")
+            .expect(200)
+            .send({title: {EN: "hi", ET: "hei"}, description: {EN: "bye", ET: "nägemist"}, pois: [0, 1]})
+            .end(function (err, res) {
+              res.body.should.be.eql({
+                title: {EN: "hi", ET: "hei"},
+                description: {EN: "bye", ET: "nägemist"},
+                pois: [0, 1],
+                id: 0
+              });
+              done(err);
+            });
         });
+
     });
   });
   describe("PUT /api/journey", function () {
@@ -136,6 +148,11 @@ describe("Journey API", function () {
     });
     it("should require id", function (done) {
       apiRequest("put", "/api/journey")
+        .expect(400, done);
+    });
+    it("should verify PoI list", function (done) {
+      apiRequest("put", "/api/journey")
+        .send({id: 0, pois: [10, 20]})
         .expect(400, done);
     });
     it("should update an existing journey", function (done) {
@@ -153,7 +170,7 @@ describe("Journey API", function () {
         });
     });
   });
- 
+
   describe("GET /api/journey", function () {
     it("should require id", function (done) {
       apiRequest("get", "/api/journey")
@@ -173,6 +190,6 @@ describe("Journey API", function () {
         });
     });
   });
- 
- 
+
+
 });
