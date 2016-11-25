e09438f3cbccfa6fecd84b649f6a7bb903dbf19f
Rezonation <TheAoihoshi@gmail.com>
2016-11-25 23:21:40 +0200

    Add coordinate selection from map for PoI and fix post and put requests. Fixes #48

--- CMS/public/js/poi.js
+++ CMS/public/js/poi.js
@@ -1,3 +1,32 @@
+/**function initialize() {
+  var loc =
+  var myOptions = {
+    zoom: 8,
+    center: loc,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  var marker = new google.maps.Marker({
+    draggable: true,
+    position: loc,
+    map: map,
+    title: "Your location"
+  });
+  google.maps.event.addListener(marker, 'dragend', function (event) {
+    Vue.set(poi, "lat", event.latLng.lat());
+    Vue.set(poi, "lot", event.latLng.lng());
+  });
+
+  google.maps.event.addListener(map, 'click', function (event) {
+    Vue.set(poi, "lat", event.latLng.lat());
+    Vue.set(poi, "lot", event.latLng.lng());
+    marker.setPosition(event.latLng);
+  });
+
+}**/
+// Based on solution from http://stackoverflow.com/questions/35053426/place-marker-on-click-google-maps-javascript-api
+
+
 var poi = new Vue({
   el: '#poi',
   data: {
@@ -12,15 +41,17 @@ var poi = new Vue({
   created: function () {
     let context = this;
     if (poi_id != -1) {
-      $.get({url: "/api/poi", data: {id: poi_id}, success: function (res) {
-        context.$set(context, "id", res.id);
-        context.$set(context, "title", res.title);
-        context.$set(context, "description", res.description);
-        context.$set(context, "lat", res.lat);
-        context.$set(context, "lon", res.lon);
-        context.$set(context, "video", res.video);
-        context.$set(context, "img", res.img);
-      }});
+      $.get({
+        url: "/api/poi", data: {id: poi_id}, success: function (res) {
+          context.$set(context, "id", res.id);
+          context.$set(context, "title", res.title);
+          context.$set(context, "description", res.description);
+          context.$set(context, "lat", res.lat);
+          context.$set(context, "lon", res.lon);
+          context.$set(context, "video", res.video);
+          context.$set(context, "img", res.img);
+        }
+      });
     }
 
 
@@ -28,35 +59,95 @@ var poi = new Vue({
   methods: {
     submitPoi: function () {
       if (poi_id == -1) {
-        $.post("/api/poi", {
+        /**$.post("/api/poi", JSON.stringify({
           title: this.title,
           description: this.description,
           lat: this.lat,
           lon: this.lon,
           video: this.video,
           img: this.img
-        }, function (result) {
-          // Should redirect back to /
-        });
-      } else {
+        }),function (result) {
+          //TODO Should redirect back to /
+        }, 'json');**/
         $.ajax({
           url: '/api/poi',
-          type: 'PUT',
-          data: {
-            id: this.id,
+          type: 'POST',
+          data: JSON.stringify({
             title: this.title,
             description: this.description,
             lat: this.lat,
             lon: this.lon,
             video: this.video,
             img: this.img
-          },
+          }),
+          dataType: 'json',
+          contentType: 'application/json',
           success: function (result) {
-            // Should redirect back to /
+            //TODO Should redirect back to /
+          }
+        });
+      } else {
+        $.ajax({
+          url: '/api/poi',
+          type: 'PUT',
+          data: JSON.stringify({
+            id: this.id,
+            title: this.title,
+            description: this.description,
+            lat: this.lat,
+            lon: this.lon,
+            video: this.video,
+
+            img: this.img
+          }),
+          dataType: 'json',
+          contentType: 'application/json',
+          success: function (result) {
+            //TODO Should redirect back to /
           }
         });
       }
     }
+  },
+  watch: {
+    lat: function (val) {
+      marker.setPosition(new google.maps.LatLng(this.lat, this.lon));
+    },
+    lon: function (val) {
+      marker.setPosition(new google.maps.LatLng(this.lat, this.lon));
+    }
   }
 
 });
+
+
+var myLatlng = new google.maps.LatLng(58.3776252, 26.7290063);
+if (poi.lat != null && poi.lon != null) {
+  myLatlng = new google.maps.LatLng(this.lat, this.lon);
+}
+
+var myOptions = {
+  zoom: 8,
+  center: myLatlng,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+var marker = new google.maps.Marker({
+  draggable: true,
+  position: myLatlng,
+  map: map,
+  title: "Your location"
+});
+
+google.maps.event.addListener(marker, 'dragend', function (event) {
+  Vue.set(poi, "lat", event.latLng.lat());
+  Vue.set(poi, "lon", event.latLng.lng());
+});
+
+google.maps.event.addListener(map, 'click', function (event) {
+  Vue.set(poi, "lat", event.latLng.lat());
+  Vue.set(poi, "lon", event.latLng.lng());
+  marker.setPosition(event.latLng);
+});
+
