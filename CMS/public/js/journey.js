d461393148c193d208b2f2c36388e2dcde7dc8c0
Rezonation <TheAoihoshi@gmail.com>
2016-11-27 23:29:05 +0200

    Setup journey script and jade for development

---
+++ CMS/public/js/journey.js
@@ -0,0 +1,12 @@
+var journey = new Vue({
+  el: '#journey',
+  data: {
+    id: null,
+    pois: null,
+    title: null,
+    description: null,
+    clicked: false,
+    delMessage: "Delete",
+    allpoi: null
+  }
+});
