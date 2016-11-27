c12eba88c3e2bac0b66c142623857f23c8405e2e
Rezonation <TheAoihoshi@gmail.com>
2016-11-28 01:52:40 +0200

    Add text fields to journey screen and hook up journey screen to API

--- CMS/public/js/journey.js
+++ CMS/public/js/journey.js
@@ -1,12 +1,97 @@
+Vue.use(VeeValidate);
+
 var journey = new Vue({
   el: '#journey',
   data: {
     id: null,
-    pois: null,
-    title: null,
-    description: null,
+    pois: [],
+    title: {EN: null, ET: null},
+    description: {EN: null, ET: null},
     clicked: false,
     delMessage: "Delete",
-    allpoi: null
+    allpois: []
+  },
+  created: function () {
+    let context = this;
+    if (journey_id != -1) {
+      $.get({
+        url: "/api/journey", data: {id: journey_id}, success: function (res) {
+          Vue.set(context, "id", res.id);
+          Vue.set(context, "pois", res.pois);
+          Vue.set(context, "title", res.title);
+          Vue.set(context, "description", res.description);
+        }
+      });
+    } else {
+      Vue.set(context, "id", journey_id);
+
+    }
+    $.get({
+      url: "/api/pois", data: {id: journey_id}, success: function (res) {
+        Vue.set(context, "allpois", res)
+      }
+    });
+  },
+  methods: {
+    submitJourney: function () {
+      this.$validator.validateAll();
+      if (this.errors.any()) {
+        return
+      }
+      if (journey_id == -1) {
+        $.ajax({
+          url: '/api/journey',
+          type: 'POST',
+          data: JSON.stringify({
+            pois: this.pois,
+            title: this.title,
+            description: this.description,
+          }),
+          dataType: 'json',
+          contentType: 'application/json',
+          success: function (result) {
+            window.location.href = '/';
+            //TODO Display errors
+          }
+        });
+      } else {
+        $.ajax({
+          url: '/api/journey',
+          type: 'PUT',
+          data: JSON.stringify({
+            id: this.id,
+            pois: this.pois,
+            title: this.title,
+            description: this.description,
+          }),
+          dataType: 'json',
+          contentType: 'application/json',
+          success: function (result) {
+            window.location.href = '/';
+            //TODO Display errors
+          }
+        });
+      }
+    },
+    del: function () {
+      if (this.clicked) {
+        $.ajax({
+          url: '/api/journey',
+          type: 'DELETE',
+          data: JSON.stringify({
+            id: this.id,
+          }),
+          dataType: 'json',
+          contentType: 'application/json',
+          success: function (result) {
+            window.location.href = '/';
+            //TODO Display errors
+          }
+        });
+      } else {
+        Vue.set(this, "delMessage", "Click again to DELETE");
+        Vue.set(this, "clicked", true);
+      }
+    }
   }
 });
