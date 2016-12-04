37c0ed18e97f064c38d426be9bef2d8bf9c315a3
Rasmus Saks <bogeyman.est@gmail.com>
2016-12-04 18:28:32 +0200

    Add titles to CMS pages

--- CMS/routes/index.js
+++ CMS/routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Aken Ajalukku'});
 });
 router.get('/poi/new', function(req, res, next) {
-  res.render('poi', { id: -1});
+  res.render('poi', {id: -1, title: 'Aken Ajalukku'});
 });
 router.get('/poi/:id', function(req, res, next) {
-  res.render('poi', { id: req.params.id});
+  res.render('poi', {id: req.params.id, title: 'Aken Ajalukku'});
 });
 router.get('/journey/new', function(req, res, next) {
-  res.render('journey', { id: -1 });
+  res.render('journey', {id: -1, title: 'Aken Ajalukku'});
 });
 router.get('/journey/:id', function(req, res, next) {
-  res.render('journey', { id: req.params.id });
+  res.render('journey', {id: req.params.id, title: 'Aken Ajalukku'});
 });
 
 module.exports = router;
